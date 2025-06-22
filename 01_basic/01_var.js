1213a2d988c4ac92336103f8aa33201f15c2952a
Aman Kumar Singh <123794954+amansingh4012@users.noreply.github.com>
2025-06-22 07:21:57 +0000

    comment config

---
+++ 01_basic/01_var.js
@@ -0,0 +1,13 @@
+const studentId = 55454
+var studentName = "AMAN SINGH"
+let studentEmail = "amansingh.as9170@email.com"
+let studentPhoneNo;
+
+
+
+studentEmail = "aman@google.com"
+studentName = "anuj"
+studentPhoneNo = "6392119961"
+
+console.table([studentId, studentName, studentEmail, studentPhoneNo])
+
