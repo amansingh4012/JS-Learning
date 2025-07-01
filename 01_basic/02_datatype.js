29a5e54aba467b78d968a5311350d98828a0ae27
Aman Kumar Singh <123794954+amansingh4012@users.noreply.github.com>
2025-07-01 17:02:06 +0000

    datatypes

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+//alert(5+6)// we are using node js ,  not browser 
+
+// code readability should be high
+
+
+console.log("hitesh")
+
+
+let name = "hitesh "
+let age = 21
+let isloggedIn = false
+
+
+
+// number => 2 to power 53
+// bigint
+//string -> ""
+// boolean --> true/false
+// null ==> standalone value
+//undefined => 
+// symbol => unnique
+
+// object
+
+console.log(typeof age);
+console.log(typeof null);
+
