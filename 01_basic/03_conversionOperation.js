9d7f05e44d1b84843ab473062d0232ff0b33108a
Aman Kumar Singh <123794954+amansingh4012@users.noreply.github.com>
2025-07-02 06:24:21 +0000

    conversionOperation

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,11 +1,11 @@
-// let score = "33abc";
+// let scor//e = "33abc";
 
-// console.log(typeof score);
+// console.log(ty//peof score);
 
 // console.log(typeof (score));
 
-//let valueInNumber = Number(score);
-//console.log(typeof (valueInNumber));
+//let valueInNumber = //Number(score);
+//console.log(typeof (va//lueInNumber));
 //console.log(valueInNumber);
 
 //"33" => 33
@@ -14,7 +14,7 @@
 
  let isLoggedIn = 1
 
- let booleanIsLoggedIn = Boolean(isLoggedIn)
+ let booleanIsLoggedIn = Boolean//(isLoggedIn)
 
  //console.log(booleanIsLoggedIn)
 
@@ -22,10 +22,40 @@
  // " " => false; "aman" => true;
 
  let someNumber = 33
- let stringNumber = String(someNumber)
+ let stringNumber = String//(someNumber)
 
- console.log(stringNumber)
- console.log(typeof stringNumber)
+ //console.log(//stringNumber)
+ //console.log(typeof stringNumber)
 
- 
+// *********************************** Operations ********************************
+
+let value = 3
+let negV//alue = -value;
+//console//.log(negValue);
+
+//console.log(3+5)
+//console.log(3-5)
+//console.log(3*5)
+//console.log(3/5)
+//console.log(5%2)
+//console.log(3**5)
+
+let str1 = "hello"
+let str2 = " Aman"
+
+let str3 = str1 + str2;
+//console.log(str3)
+
+//console.log("2" + 4)
+//console.log(4 + "2")
+//console.log(2 + "2" + 2)
+
+let c = 1;
+console.log(c)
+
+console.log(c++)
+console.log(c)
+
+
+
 
