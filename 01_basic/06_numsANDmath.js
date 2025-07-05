becf1faecd48732cc0c8ef2fd435e491a6c3c597
Aman Kumar Singh <123794954+amansingh4012@users.noreply.github.com>
2025-07-05 09:39:23 +0000

    06_

---
+++ 01_basic/06_numsANDmath.js
@@ -0,0 +1,34 @@
+/** 
+const score = 100
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const other = 1234.8956;
+// console.log(other.toPrecision(5))
+
+const hundered = 1000000000000;
+// console.log(hundered.toLocaleString('en-IN'));  **/
+
+
+// *********** MATHS *******************
+
+// console.log(Math)
+// console.log(Math.abs(-5))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.3))
+// console.log(Math.max(5,6,1,3,9,7))
+// console.log(Math.min(5,6,7,1,2))
+
+console.log(Math.random());
+console.log(Math.floor(Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min))+ min)
