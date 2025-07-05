ec84d9963b99740ec77adaeffce2bcb47b45b63d
Aman Kumar Singh <123794954+amansingh4012@users.noreply.github.com>
2025-07-05 06:19:33 +0000

    datatype-summar

---
+++ 01_basic/comparsion.js
@@ -0,0 +1,13 @@
+//console.log(1>2);
+//console.log(1<2);
+//console.log(1>=2);
+//console.log(1<=2);
+//console.log(1==2);
+//console.log(1!=2);
+
+console.log(null>0)
+//console.log(null<0)
+//console.log(null==0)
+//console.log(null<=0)
+
+// === => check the data type also
