b7659a6caf518e14fbb62e61cf8992bc6285c4e7
Aman Kumar Singh <123794954+amansingh4012@users.noreply.github.com>
2025-07-05 06:57:01 +0000

    datatype-summary.js

--- 01_basic/datatype-summary.js
+++ 01_basic/datatype-summary.js
@@ -2,7 +2,7 @@
 
 //  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
 
-const score = 100
+/**const score = 100
 const scoreValue = 100.3
 
 const isLoggedIn = false
@@ -32,6 +32,36 @@ const myFunction = function(){
     console.log("Hello world");
 }
 
-console.log(typeof anotherId);
+console.log(typeof anotherId); **/
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+//***************************** */
+// stack(Primitive) => copy the content 
+
+// Heap (Non- Primitive) => reference the value 
+
+let myYoutubename = "amansingh-code"
+let yourYoutubename = myYoutubename
+
+yourYoutubename = "anujsingh-code"
+
+console.log(myYoutubename)
+console.log(yourYoutubename)
+
+let userOne = {
+    email : "aman@google.com",
+    upi : "user@icc"
+};
+
+let userTwo = userOne;
+
+userTwo.email = "anuj@google.com"
+
+console.log(userOne)
+console.log(userTwo)
+
+
+
