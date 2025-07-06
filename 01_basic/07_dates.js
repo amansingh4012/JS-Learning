6241cc98b701203382fe1daa9b10d199b0455be1
Aman Kumar Singh <123794954+amansingh4012@users.noreply.github.com>
2025-07-06 11:47:12 +0000

    date

---
+++ 01_basic/07_dates.js
@@ -0,0 +1,30 @@
+// dates
+/** 
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toString())
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+console.log(myDate.toJSON())
+console.log(typeof myDate) **/
+
+//let myCreatedDate = new Date(2023, 0, 23)
+//let myCreatedDate = new Date(2023, 0, 23, 15, 44)
+//let myCreatedDate = new Date("2023-05-01")
+let myCreatedDate = new Date("05-22-2023")
+
+//console.log(myCreatedDate.toLocaleString());
+
+
+//let myTimeStamp = Date.now()
+//console.log(myTimeStamp)
+
+//console.log(myCreatedDate.getTime())
+//console.log(Math.floor(Date.now()/1000))
+
+let newDate = new Date();
+console.log(newDate.getMonth()+1)
+
+newDate.toLocaleString('default', {
+    weekday: "long",
+})
