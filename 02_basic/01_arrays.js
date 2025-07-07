71bce63d48ba562998e06ed0a19b94e3dce09d12
Aman Kumar Singh <123794954+amansingh4012@users.noreply.github.com>
2025-07-07 16:01:40 +0000

    01_arrays

---
+++ 02_basic/01_arrays.js
@@ -0,0 +1,44 @@
+// array
+
+const myArr = [0,1,2,3, 4, 5]
+const myHeros = ["aman", "Anuj"]
+
+const myArr2 = new Array(1,2,3,4)
+
+console.log(myHeros[1])
+
+// Array methods 
+
+//myArr.push(6,7)
+//myArr.pop()
+//myArr.unshift(5)
+//myArr.shift()
+
+//console.log(myArr)
+
+//console.log(myArr.includes(3))
+//console.log(myArr.indexOf(3))
+
+//const newArr = myArr.join()
+//console.log( myArr)
+//console.log( newArr)
+
+// slice,, splice
+
+console.log("A" , myArr)
+
+const myArrn1 = myArr.slice(1,3)
+console.log(myArrn1)
+console.log("B" , myArr)
+
+const myArrn2 = myArr.splice(1,3)
+console.log(myArrn2)
+console.log("C", myArr)
+
+
+
+
+
+
+
+
